6536983771855b2149f51a2b340d63573aa4fd4d
Cristiano <cristianodasilva.azevedo@gmail.com>
2016-09-07 12:19:41 -0300

    Ultimo commit 12:20

--- www/js/app.js
+++ www/js/app.js
@@ -7,58 +7,78 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  .state('tab', {
-      url: '/tab',
-      abstract: true,
-      templateUrl: 'templates/tabs.html'
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
     })
 
-  .state('tab.primeiro', {
-    url: '/primeiro-passo',
-    views: {
-      'main': {
-        templateUrl: 'templates/primeiro-passo.html',
-        controller: 'Main'
-      }
-    }
-  })
+    .config(function ($stateProvider, $urlRouterProvider) {
 
-  .state('tab.segundo', {
-      url: '/segundo-passo',
-      views: {
-        'main': {
-          templateUrl: 'templates/segundo-passo.html',
-          controller: 'Main'
-        }
-      }
-    })
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
+
+            .state('tab', {
+                url: '/tab',
+                abstract: true,
+                templateUrl: 'templates/tabs.html'
+            })
+
+            .state('tab.primeiro', {
+                url: '/primeiro-passo',
+                views: {
+                    'main': {
+                        templateUrl: 'templates/primeiro-passo.html',
+                        controller: 'Main'
+                    }
+                }
+            })
+
+            .state('tab.segundo', {
+                url: '/segundo-passo',
+                views: {
+                    'main': {
+                        templateUrl: 'templates/segundo-passo.html',
+                        controller: 'Main'
+                    }
+                }
+            })
+
+            .state('tab.timeline', {
+                url: '/timeline',
+                views: {
+                    'main': {
+                        templateUrl: 'templates/timeline.html',
+                        controller: 'Timeline'
+                    }
+                }
+            })
+
+            .state('tab.form', {
+                url: '/tab-form',
+                views: {
+                    'main': {
+                        templateUrl: 'templates/form.html',
+                        controller: 'Form'
+                    }
+                }
+            })
 
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/tab/primeiro-passo');
 
-});
+    });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,39 @@
 angular.module('starter.controllers', [])
 
 
-    .controller('Main', function ($scope, $state, $http) {
+    .controller('Main', function ($scope, $state, $http, $window, $ionicPopup, $timeout, $rootScope) {
 
-        var url = 'http://localhost:8080';
+        $scope.bairros = [];
+        $rootScope.usuario = {bairro: '', bairros: []};
+
+        var localStorage = $window.localStorage;
 
         $scope.confirmarEnderecoUsuario = function () {
             $state.go('tab.segundo');
         };
 
-        $scope.confirmarEnderecosInteresse = function () {
-
+        $scope.addBairro = function (id) {
+            $rootScope.usuario.bairro = id;
+            localStorage['id'] = id;
         }
 
-        $scope.bairros = [];
-        $scope.usuario = {bairro: '', bairros: []}
+        $scope.addBairros = function (id) {
+            $rootScope.usuario.bairros.push(id);
+        }
+
+        $scope.confirmarEnderecosInteresse = function () {
+            $rootScope.usuario.bairro = localStorage['id'];
+            $http.post('http://localhost:8080/api/usuario', $rootScope.usuario).then(function success(result) {
+                localStorage['hash'] = result.data;
+                $ionicPopup.alert({
+                    title: 'Obrigado',
+                    template: 'Cadastro realizado com sucesso.'
+                });
+                $state.go('tab.timeline');
+            }, function error(result) {
+                console.log(result);
+            });
+        }
 
         $http.get('http://localhost:8080/api/bairro').then(function success(result) {
             $scope.bairros = result.data;
@@ -23,26 +42,37 @@ angular.module('starter.controllers', [])
         });
     })
 
-    .controller('NotificCtrl', function ($scope, $ionicActionSheet) {
+    .controller('Timeline', function ($scope, $http, $window, $state) {
+        var localStorage = $window.localStorage;
+        $scope.time = [];
 
-        $scope.mostrarOcorrencia = function () {
+        $http.get('http://localhost:8080/api/usuario/timeline/' + localStorage['hash']).then(function success(result) {
+            $scope.timeline = result.data;
+        }, function error(result) {
+            console.log(result);
+        });
 
-            $ionicActionSheet.show({
-                titleText: 'Ocorrências',
-                buttons: [
-                    {text: '<i class="icon flaticon-gun"></i> Assalto'},
-                    {text: '<i class="icon flaticon-car-insurance"></i> Acidente'},
-                    {text: '<i class="icon flaticon-skull-and-bones"></i> Homicídio'},
-                    {text: '<i class="icon flaticon-group"></i> Tumulto'}
-                ],
-                cancelText: 'Cancelar',
-                cancel: function () {
-                    console.log('CANCELLED');
-                },
-                buttonClicked: function (index) {
-                    console.log('BUTTON CLICKED', index);
-                    return true;
-                }
+        $scope.cadatrarOcorrencia = function () {
+            $state.go('tab.form');
+        }
+    })
+
+    .controller('Form', function ($scope, $http, $state, $ionicPopup) {
+        $scope.ocorrencia = {bairro: '', tipo_ocorrencia: '', local: '', descrica: ''};
+        $scope.bairros = [];
+
+        $http.get('http://localhost:8080/api/bairro').then(function success(result) {
+            $scope.bairros = result.data;
+        }, function error(data) {
+            console.log(data);
+        });
+
+        $scope.cadatrarOcorrenciaForm = function () {
+            $ionicPopup.alert({
+                title: 'Aviso',
+                template: 'Ocorrência cadastrada com sucesso.'
             });
-        };
+            $state.go('tab.timeline');
+        }
+
     });
